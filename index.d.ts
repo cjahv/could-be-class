8ec04762b7a15a88802d835043543c8d5a5d4e31
Ayon Lee <i@hyurl.com>
2019-01-19 10:48:37 +0800

    fix type error

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,5 @@
  * @param strict If `true`, when testing ES5 class, will check if its name's 
  *  first character is upper-cased.
  */
-export function isClass(obj: any, strict: boolean): boolean;
+export function isClass(obj: any, strict?: boolean): boolean;
 export default isClass;
