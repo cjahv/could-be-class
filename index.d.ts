bfda2868f5080942b6ad91b500dd581337828315
Ayon Lee <i@hyurl.com>
2020-04-04 14:29:21 +0800

    upgrade to 0.2, remove strict mode and improve accuracy

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
 /**
  * Checks if an object could be an instantiable class.
- * @param strict If `true`, when testing ES5 class, will check if its name's 
- *  first character is upper-cased.
  */
-export function isClass(obj: any, strict?: boolean): boolean;
+export function isClass(obj: any): obj is new (...args: any[]) => any;
 export default isClass;
