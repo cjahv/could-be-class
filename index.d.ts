cd2727102f96a6c8eb8bb7bddd4dff9c9956c5ed
cjahv <me@cjahv.com>
2021-08-06 18:18:27 +0800

    should be couldBeClass

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
  * Checks if an object could be an instantiable class.
  */
-export function isClass(obj: any): obj is new (...args: any[]) => any;
-export default isClass;
+export function couldBeClass(obj: any): obj is new (...args: any[]) => any;
+export default couldBeClass;
